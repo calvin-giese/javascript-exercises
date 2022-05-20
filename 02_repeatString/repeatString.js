3d9b1683bd21318752f42c81ffb24efbaebeef52
calvin-giese <cgiese@fieldsafesolutions.com>
2022-05-20 14:08:11 -0600

    feat: testing excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var appendedString = "";
+    if (num >= 0){
+        for (let i = 0; i < num; i++){
+            appendedString += string;
+        }
+    }
+    else {
+        appendedString = 'ERROR';
+    }
+    return appendedString;
 };
 
 // Do not edit below this line
