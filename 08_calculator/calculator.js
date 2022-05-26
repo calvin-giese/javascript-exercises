5c88e0994d662913a17b9ea27efa7ed6a0c94f94
calvin-giese <cgiese@fieldsafesolutions.com>
2022-05-26 11:20:34 -0600

    feat: testing for excercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  var sum = 0;
+	for(number of numbers){
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  var total = 0;
+	for(var i = 0; i < numbers.length; i++){
+    if(i == 0){
+      total = numbers[i];
+    }
+    else{
+      total = total * numbers[i];
+    }
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	return Math.pow(number, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	var total = 1;
+  for(var i = 1; i <= number; i++){
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
