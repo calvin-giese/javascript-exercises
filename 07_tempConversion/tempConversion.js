ba200757562c8407075a41fcf60021391db9cb16
calvin-giese <cgiese@fieldsafesolutions.com>
2022-05-26 11:07:29 -0600

    feat: testing for excercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  var celsius = (fahrenheit - 32) * 0.5556;
+  return formatTemp(celsius);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  var fahrenheit = (celsius * 1.8) + 32;
+  return formatTemp(fahrenheit);
 };
+
+function formatTemp(temp){
+  temp = temp.toFixed(1);
+  return parseFloat(temp);
+}
 
 // Do not edit below this line
 module.exports = {
