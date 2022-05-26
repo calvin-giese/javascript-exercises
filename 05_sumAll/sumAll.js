37e1055b67619a1603b64de0454648b408e16481
calvin-giese <cgiese@fieldsafesolutions.com>
2022-05-26 10:44:56 -0600

    feat: testing for excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(!(typeof(start) == "number") || !(typeof(end) == "number")){
+        return "ERROR";
+    }
 
+    if(start < 0 || end < 0){
+        return "ERROR";
+    }
+
+    if(start > end){
+        let temp;
+        temp = start;
+        start = end;
+        end = temp;
+    }
+
+    var sum = 0;
+
+    for(var i = start; i <= end; i++){
+        sum += i;
+    }
+
+    return sum;
 };
+
+function isNumber(value){
+    if(typeof(value) == Number){
+        return true;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
