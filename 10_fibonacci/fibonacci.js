407a03f30295170105b28e699b7b556f26d38182
calvin-giese <cgiese@fieldsafesolutions.com>
2022-05-30 15:36:35 -0600

    feat: testing for excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    var fibonacci = [];
 
+    if (place < 0) {
+        return "OOPS"
+    } else {
+        for (let i = 0; i < place; i++) {
+            if (i < 2) {
+                fibonacci[i] = 1;
+            } else {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i-2];
+            }
+        }
+        return fibonacci[place - 1];
+    }
 };
 
 // Do not edit below this line
