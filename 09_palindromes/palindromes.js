c394440786a847464f2750f0f89b2937a1a472da
calvin-giese <cgiese@fieldsafesolutions.com>
2022-05-30 15:30:31 -0600

    feat: testing for excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    var reverse = '';
+    var test = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        if (word.at(i).match(/[a-zA-Z]/i)) {
+            reverse += word.at(i).toLowerCase();
+        }
+    }
 
+    for (let i = 0; i < word.length; i++) {
+        if (word.at(i).match(/[a-zA-Z]/i)) {
+            test += word.at(i).toLowerCase();
+        }
+    }
+
+    if (reverse == test) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
