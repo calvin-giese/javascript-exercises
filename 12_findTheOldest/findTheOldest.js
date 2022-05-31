4a5bd20b3a8a3f1c7a81612c3fee93d3d4bb0178
calvin-giese <cgiese@fieldsafesolutions.com>
2022-05-31 08:20:42 -0600

    feat: testing for excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    var oldestAge = 0;
+    var oldestAgeIndex = 0;
 
+    for (let i = 0; i < people.length; i++) {
+        let yearOfBirth = people[i].yearOfBirth;
+        let yearOfDeath = people[i].yearOfDeath;
+        let todaysYear = new Date().getFullYear();
+        let age = 0;
+
+        if (yearOfDeath > 0) {
+            age = yearOfDeath - yearOfBirth;
+        } else {
+            age = todaysYear - yearOfBirth;
+        }
+
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestAgeIndex = i;
+        }
+    }
+
+    return people[oldestAgeIndex];
 };
 
 // Do not edit below this line
